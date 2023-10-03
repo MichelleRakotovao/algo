4807d6493fe1ec8f78091f7f0c46690e59967ba4
Michelle <rakotovaomichelle7@gmail.com>
2023-10-03 10:19:46 +0300

    réccupération des données dans le fichier .txt

--- tpAlgo/Program.cs
+++ tpAlgo/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ExoAlgo{
+    class ExoAlgo{
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("Begin");
+           string nomFichier="liste_francais.txt";
+           try{
+            string[] lignes = File.ReadAllLines(nomFichier);
+            List<string> mots=new List<string>(lignes);
+            System.Console.WriteLine(mots[100]);//abrité
+           }catch(Exception e){
+            System.Console.WriteLine(e.Message);
+           }
+        }
+    }
+}
